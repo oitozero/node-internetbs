b96b4fdd85fd49ddaa54c55bf79987eec157c6a4
Pedro Sampaio <pedro@clevertech.biz>
2016-07-26 13:06:51 +0100

    0.0.1

---
+++ README.md
@@ -0,0 +1,106 @@
+# node-internetbs
+
+Node.js library for [Internet.bs Reseller/Registrar Domain Name API](https://www.internetbs.net/ResellerRegistrarDomainNameAPI/).
+
+## Installation
+
+`npm install internetbs`
+
+## Usage overview
+
+### new Internetbs(api_key, password [, sandbox])
+
+	var Internetbs = require('internetbs'),
+	  internetbs = new Internetbs('testapi', 'testpass', true);
+
+	internetbs.domains.check('example.com', function(err, res) {
+	  console.log(res);
+	});
+
+## [Domain Methods](https://www.internetbs.net/ResellerRegistrarDomainNameAPI/)
+
+### internetbs.domains.check(domain, callback)
+
+Checks the availability of a domain
+
+	internetbs.domains.check('example.com', function(err, res) {
+		console.log(res);
+	});
+
+### internetbs.domains.create(domain [, years] [, params], callback)
+
+Registers a new domain name. Optionally include `years` and `params` to set additional [required params](https://www.internetbs.net/ResellerRegistrarDomainNameAPI/). If `params` fields are not set, values from `internetbs.setDefaults` will be used.
+
+	internetbs.domains.create('example.com', '1Y', function(err, res) {
+		console.log(res);
+	});
+
+	internetbs.domains.create('example.com', { Registrant_FirstName: 'Pedro', ..... Billing_Email: 'pedro@oitozero.com' }, function(err, res) {
+		console.log(res);
+	});
+
+### internetbs.domains.getInfo(domain, callback)
+
+Returns information about the requested domain.
+
+	internetbs.domains.getInfo('example.com', function(err, res) {
+	  console.log(res);
+	});
+
+### internetbs.domains.getList([params,] callback)
+
+Returns a list of domains for an account. Set [optional params](https://www.internetbs.net/ResellerRegistrarDomainNameAPI/) to filter results.
+
+	internetbs.domains.getList(function(err, res) {
+	  console.log(res);
+	});
+
+## [DNS Methods](https://www.internetbs.net/ResellerRegistrarDomainNameAPI/)
+
+### internetbs.domains.dns.add(domain, type, value, params, callback)
+
+Add a new DNS record to a specific zone (domain).
+
+	internetbs.domains.dns.add('www.example.com', 'A', '192.168.1.1', {}, function(err, res) {
+	  console.log(res);
+	});
+
+### internetbs.domains.dns.remove(domain, type, params, callback)
+
+Remove a DNS record from a specific zone..
+
+	internetbs.domains.dns.remove('www.example.com', 'A', {}, function(err, res) {
+	  console.log(res);
+	});
+
+### internetbs.domains.dns.update(domain, type, newValue, params, callback)
+
+Update an existing DNS record.
+
+	internetbs.domains.dns.update('www.example.com', 'A', '192.168.1.2', {}, function(err, res) {
+	  console.log(res);
+	});
+
+### internetbs.domains.dns.getList(domain, type, callback)
+
+Retrieve the list of DNS records for a specific domain.
+
+	internetbs.domains.dns.setCustom('smithfam.net', ['dns1.stabletransit.com', 'dns2.stabletransit.com'], function(err, res) {
+	  console.log(res);
+	});
+
+## TODO
+
+* Implement missing Domain methods
+* Implement Nameservers (host) methods
+* Implement Domain forwarding methods
+* Implement Account related methods
+
+
+## Author
+
+[Pedro Sampaio](http://twitter.com/oitozero) ([pedro@oitozero.com](mailto:pedro@oitozero.com)).
+
+## License
+
+This project is [UNLICENSED](http://unlicense.org/) and not endorsed by or affiliated with [Internet.bs](https://internetbs.net/).
---
+++ internetbs.js
@@ -0,0 +1,141 @@
+var request = require('request');
+var qs = require('querystring');
+var util = require('util');
+
+var internetbs = function(api_key, password, sandbox) {
+    this.api_key = (sandbox ? api_key : 'testapi');
+    this.password = (sandbox ? password : 'password');
+    this.endpoint = 'https://' + (sandbox ? 'testapi' : 'api') + '.internet.bs';
+    this.defaults = {};
+};
+internetbs.prototype = {
+    get domains() {
+        var instance = this;
+        return {
+            check: function(domain, callback) {
+                return instance.command('Domain/Check', { domain: domain }, callback);
+            },
+            create: function(domain, years, params, callback) {
+                if(!domain)
+                throw new Error('You must include a domain name.');
+                var options = instance.defaults, k, y = parseInt(years);
+                if('[object Function]' == Object.prototype.toString.call(years)) {
+                    callback = years;
+                    y = 1;
+                    params = {};
+                }
+                else if('[object Object]' == Object.prototype.toString.call(years)) {
+                    params = years;
+                    y = 1;
+                }
+                if('[object Function]' == Object.prototype.toString.call(params)) {
+                    callback = params;
+                    params = {};
+                }
+                for(k in params) {
+                    if(params.hasOwnProperty(k)) {
+                        options[k] = params[k];
+                    }
+                }
+                options.Domain = domain;
+                options.Period = years; // 1Y, 2Y up to 10Y
+                return instance.command('Domain/Create', options, callback);
+            },
+            getInfo: function(domain, callback) {
+                if(!domain)
+                    throw new Error('You must include a domain name.');
+                return instance.command('Domain/Info', { Domain: domain }, callback);
+            },
+            getList: function(params, callback) {
+                if('[object Function]' == Object.prototype.toString.call(params)) {
+                    callback = params;
+                    params = {};
+                }
+                return instance.command('Domain/List', params, callback);
+            },
+            ns: {
+                create: function(domain, host, callback) {
+                    return instance.command('Domain/Host/Create', {Domain: domain, Host: host}, callback);
+                },
+                getInfo: function(host, callback) {
+                    return instance.command('Domain/Host/Info', {host: host}, callback);
+                },
+                getList: function(domain, callback) {
+                    return instance.command('Domain/Host/List', {Domain: domain}, callback);
+                }
+            },
+            dns: {
+                add: function(domain, type, value, params, callback) {
+                    var options = instance.defaults;
+                    options.FullRecordName = domain;
+                    options.Type = type; //  A, AAAA, DYNAMIC, CNAME, MX, SRV, TXT and NS
+                    options.Value = value;
+                    for(k in params) {
+                        if(params.hasOwnProperty(k)) {
+                            options[k] = params[k];
+                        }
+                    }
+                    return instance.command('Domain/DnsRecord/Add', options, callback);
+                },
+                remove: function(domain, type, params, callback) {
+                    var options = instance.defaults;
+                    options.FullRecordName = domain;
+                    options.Type = type; //  A, AAAA, DYNAMIC, CNAME, MX, SRV, TXT and NS
+                    for(k in params) {
+                        if(params.hasOwnProperty(k)) {
+                            options[k] = params[k];
+                        }
+                    }
+                    return instance.command('Domain/DnsRecord/Remove', options, callback);
+                },
+                update: function(domain, type, value, params, callback) {
+                    var options = instance.defaults;
+                    options.FullRecordName = domain;
+                    options.Type = type; //  A, AAAA, DYNAMIC, CNAME, MX, SRV, TXT and NS
+                    options.NewValue = value;
+                    for(k in params) {
+                        if(params.hasOwnProperty(k)) {
+                            options[k] = params[k];
+                        }
+                    }
+                    return instance.command('Domain/DnsRecord/Update', options, callback);
+                },
+                getList: function(domain, type, callback){
+                    var options = instance.defaults;
+                    options.Domain = domain;
+                    if(type !== null){
+                        options.FilterType = type;
+                    }
+                    return instance.command('Domain/DnsRecord/List', options, callback);
+                }
+            }
+        };
+    },
+    command: function(cmd, params, callback, method) {
+        var instance = this;
+        params.ApiKey = this.api_key;
+        params.Password = this.password;
+        // params.ClientIp = this.client_ip;
+        params.ResponseFormat = 'JSON';
+        var options = {
+            method: method || 'GET',
+            uri: this.endpoint + '/' + cmd
+        };
+        params = qs.stringify(params);
+        if('POST' == method) {
+            options.body = params;
+        }
+        else {
+            options.uri += '?' + params;
+        }
+        console.log('options:', options);
+        request(options, function(err, res, body) {
+            // callback(err, instance.parseResponseBody(body));
+            // console.log('body:', body);
+            callback(err, body);
+        });
+        return this;
+    }
+};
+
+module.exports = internetbs;
